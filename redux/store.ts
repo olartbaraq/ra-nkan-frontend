9175c6ab04bb86b39086eb94e52e25677ee9b16c
mubaraq akanbi <akanbi.ges201@gmail.com>
2024-03-29 07:48:27 +0100

    fix: replace Preloadedstate with Partial

--- redux/store.ts
+++ redux/store.ts
@@ -1,21 +1,21 @@
-import { PreloadedState, combineReducers, configureStore } from '@reduxjs/toolkit';
-import userReducer from '../reduxfeatures/userSlice'
-import itemReducer from '../reduxfeatures/itemSlice'
-import shippingReducer from '@/reduxfeatures/ShippingAdress';
-import storage from 'redux-persist/lib/storage';
-import { persistReducer, persistStore } from 'redux-persist';
-import autoMergeLevel2 from 'redux-persist/es/stateReconciler/autoMergeLevel2';
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userReducer from "../reduxfeatures/userSlice";
+import itemReducer from "../reduxfeatures/itemSlice";
+import shippingReducer from "@/reduxfeatures/ShippingAdress";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import autoMergeLevel2 from "redux-persist/es/stateReconciler/autoMergeLevel2";
 
 const persistConfig = {
-  key: 'root',
+  key: "root",
   storage,
-  stateReconciler: autoMergeLevel2
+  stateReconciler: autoMergeLevel2,
 };
 
 const rootReducer = combineReducers({
   users: userReducer,
   items: itemReducer,
-  shipping: shippingReducer
+  shipping: shippingReducer,
   // auth: authReducer,
   // userProfile: userProfileReducer
 });
@@ -26,29 +26,25 @@ const persistedReducer = persistReducer<ReturnType<typeof rootReducer>>(
 );
 
 export const Store = configureStore({
-  reducer: persistedReducer
+  reducer: persistedReducer,
 });
 
 export const persistor = persistStore(Store);
 
-export const setupStore = (preloadedState?: PreloadedState<RootState>) => {
-		return Store;
+export const setupStore = (preloadedState?: Partial<RootState>) => {
+  return Store;
 };
 
-export const createPreloadedState = (
-  customState: Partial<RootState>
-): PreloadedState<RootState> => {
-	return {
-		users: { ...Store.getState().users, ...customState.users },
-		items: { ...Store.getState().items, ...customState.items },
-		shipping: { ...Store.getState().items, ...customState.shipping },
-	};
+export const createPreloadedState = (customState: Partial<RootState>) => {
+  return {
+    users: { ...Store.getState().users, ...customState.users },
+    items: { ...Store.getState().items, ...customState.items },
+    shipping: { ...Store.getState().items, ...customState.shipping },
+  };
 };
-
 
 export type AppDispatch = typeof Store.dispatch;
 export type AppStore = ReturnType<typeof setupStore>;
 export type RootState = ReturnType<typeof Store.getState>;
-
 
 // https://blog.logrocket.com/using-typescript-redux-toolkit/
